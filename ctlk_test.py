2c8a4b98b5e91b59ca52c97d0f8e73f61bbbabd1
William Crawford <crawfowa@gmail.com>
2024-01-01 21:11:11 -0800

    Working example of jv replacement

--- ctlk_test.py
+++ ctlk_test.py
@@ -1,4 +1,16 @@
-from cltk.corpus.utils.importer import CorpusImporter
-corpus_importer = CorpusImporter("latin")
-print(corpus_importer.list_corpora)
-corpus_importer.import_corpus("latin_models_cltk")
+from cltk.data.fetch import FetchCorpus
+from cltk import NLP
+from cltk.core.data_types import Doc
+
+import cltk.text.lat as jv
+
+with open('data/pl.txt', "r") as f:
+    text = f.read()
+print(text)
+
+print(text[0:14])
+print(text[1305:1330])
+
+text = jv.replace_jv(text)
+print(text[0:14])
+print(text[1305:1330])
